00e6960978e313f96e196ca7d729e45da9dd8020
d0vid9 <64305701+baddav@users.noreply.github.com>
2025-02-02 14:54:25 +0100

    Update footer und header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import {Component} from '@angular/core';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
+  imports: [HeaderComponent, FooterComponent],
   template: `
 
     <!-- header -->
